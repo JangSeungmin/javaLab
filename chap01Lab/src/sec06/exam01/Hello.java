eb8cc6057c0c95f6f784bd595d99f14e67cb69ce
장승민 <@DESKTOP-91GQM0M>
2021-03-29 11:09:03 +0900

    Chap01Lab

---
+++ chap01Lab/src/sec06/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec06.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÀÌ¸§ : Àå½Â¹Î");
+		System.out.println("ÀüÈ­¹øÈ£ : 010-1231-1221");
+		System.out.println("Èñ¸Á»çÇ× : Ã¹¼ö¾÷~");
+	}
+
+}
