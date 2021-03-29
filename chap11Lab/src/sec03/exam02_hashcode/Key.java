97e5abc48e6923ff74bc21126adc2882417f0dd1
장승민 <@DESKTOP-91GQM0M>
2021-03-29 17:22:44 +0900

    chap11 실습

---
+++ chap11Lab/src/sec03/exam02_hashcode/Key.java
@@ -0,0 +1,23 @@
+package sec03.exam02_hashcode;
+
+public class Key {
+	public int number;
+	
+	public Key(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	
+}
